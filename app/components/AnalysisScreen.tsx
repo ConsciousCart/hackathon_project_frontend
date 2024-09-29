11d095bd5c765c61240c979083deea7166f7274d
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2024-09-29 21:41:43 +0530

    Developed the analysis page and used placeholder data

--- app/components/AnalysisScreen.tsx
+++ app/components/AnalysisScreen.tsx
@@ -1,10 +1,18 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { TypographyH2 } from "@/components/ui/Typography/TypographyH2";
 import { TypographyP } from "@/components/ui/Typography/TypographyP";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@/components/ui/table";
+import { TypographyH4 } from "@/components/ui/Typography/TypographyH4";
 
 const AnalysisScreen = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 h-full">
       <Card className="bg-transparent text-white">
         <CardHeader>
           <CardTitle>
@@ -22,6 +30,39 @@ const AnalysisScreen = () => {
           </TypographyP>
         </CardContent>
       </Card>
+
+      <div className="my-5">
+        <TypographyH4 className="font-bold tracking-wide border-none text-white">Allowed vs Not Allowed</TypographyH4>
+        <Table className="text-white w-max mx-auto">
+
+            <TableRow>
+              <TableHead className="w-[100px]">Allowed</TableHead>
+              <TableHead className="text-left">Not Allowed</TableHead>
+            </TableRow>
+          <TableBody>
+            <TableRow>
+              <TableCell>One</TableCell>
+              <TableCell className="ml-auto">Two</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>One</TableCell>
+              <TableCell className="ml-auto">Two</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>One</TableCell>
+              <TableCell className="ml-auto">Two</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>One</TableCell>
+              <TableCell className="ml-auto">Two</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>One</TableCell>
+              <TableCell className="ml-auto">Two</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
