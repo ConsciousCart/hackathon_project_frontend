085f7448a1f79762331bfd7a7d9407a1d899c4cb
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2024-09-30 14:51:44 +0530

    The screens are auto sized (height) ib large screens

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MobileView from "./components/MobileView";
 function Page() {
   return (
     <div className=" flex overflow-hidden flex-col items-stretch justify-center">
-      <main className="overflow-hidden lg:h-[640px] flex flex-col w-screen lg:my-10 items-center justify-center space-y-8 lg:py-10">
+      <main className="overflow-hidden lg:h-auto flex flex-col w-screen lg:my-10 items-center justify-center space-y-8 lg:py-10">
       <MobileView/>
       </main>
     </div>
