9819adde1635458c4c61d55a68de86815a62ab04
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2024-10-01 21:18:24 +0530

    add pie styling

--- app/components/NutrispectrumPieChart.tsx
+++ app/components/NutrispectrumPieChart.tsx
@@ -55,9 +55,11 @@ const NutrientDensityAnalysisChart: React.FC<Props> = ({ products }) => {
       <ResponsivePie
         data={pieChartData}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.5}
-        padAngle={0.7}
-        cornerRadius={3}
+        startAngle={0}
+        endAngle={360}
+        innerRadius={0}
+        padAngle={0}
+        cornerRadius={0}
         activeOuterRadiusOffset={8}
         borderWidth={1}
         borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
